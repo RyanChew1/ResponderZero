7721b163174335f6d773962d68433bc531d6fc5c
Ryan Chew <113740030+RyanChew1@users.noreply.github.com>
2024-07-08 14:54:45 -0700

    Components+update assets

--- constants/images.js
+++ constants/images.js
@@ -3,6 +3,7 @@ import logoSquare from "../assets/images/LogoSquare.png";
 import logoCircle from "../assets/images/LogoCircle.png";
 import logoNoBg from "../assets/images/LogoCircleNoBG.png";
 import logoText from "../assets/images/LogoText.png";
+import logoRounded from "../assets/images/LogoSquareRounded.png";
 import empty from "../assets/images/empty.png";
 
-export default { profile, thumbnail, cards, path, logoSquare, logoCircle, logoNoBg, logoText, empty };
+export default { profile, thumbnail, cards, path, logoSquare, logoCircle, logoNoBg, logoText, logoRounded, empty };
