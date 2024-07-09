6f51badac3542ef14927e3ef2f826e3fdcf1c20d
Ryan Chew <113740030+RyanChew1@users.noreply.github.com>
2024-07-09 14:57:47 -0700

    logs page

---
+++ app/(tabs)/logs.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const logs = () => {
+  return (
+    <View>
+      <Text>logs</Text>
+    </View>
+  )
+}
+
+export default logs
